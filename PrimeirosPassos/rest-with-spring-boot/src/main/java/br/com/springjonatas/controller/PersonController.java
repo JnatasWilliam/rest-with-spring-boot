9a01b8ccbd6d19aa4ea45c05d674de1c7de92956
jonatas <jonatas.santos@sigma.com.br>
2024-09-22 17:10:23 -0300

    Terminando CRUD sem banco de dados, por isso usando Mocks ainda

--- PrimeirosPassos/rest-with-spring-boot/src/main/java/br/com/springjonatas/controller/PersonController.java
+++ PrimeirosPassos/rest-with-spring-boot/src/main/java/br/com/springjonatas/controller/PersonController.java
@@ -1,8 +1,11 @@
 package br.com.springjonatas.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +21,28 @@ public class PersonController {
 	private PersonServices service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") String id)
-			throws Exception {
+	public Person findById(@PathVariable(value = "id") String id) throws Exception {
 
 		return service.findById(id);
+
+	}
+
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+
+		return service.findAll();
+	}
+
+	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+
+		return service.create(person);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+
+		return service.update(person);
 
 	}
 }
--- PrimeirosPassos/rest-with-spring-boot/src/main/java/br/com/springjonatas/services/PersonServices.java
+++ PrimeirosPassos/rest-with-spring-boot/src/main/java/br/com/springjonatas/services/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.springjonatas.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -9,24 +11,64 @@ import br.com.springjonatas.model.Person;
 
 @Service
 public class PersonServices {
-	
+
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
-	
-	//Criando um mock para encontrar pessoas
+
+
+	// Criando um mock para encontrar uma pessoa	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<>();
+
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+
+		return persons;
+	}
+
+	// Criando um mock para encontrar uma pessoa
 	public Person findById(String id) {
-		
+
 		logger.info("Finding one person");
-		
-		Person person =  new Person();
+		Person person = new Person();
 
 		person.setID(counter.incrementAndGet());
 		person.setFirstName("Jonatas");
 		person.setLastName("Santos");
 		person.setAddress("Alameda dos Guatás");
 		person.setGender("Male");
+
+		return person;
+	}
+
+	public Person create(Person person) {
+
+		logger.info("Creating one person");
 		
 		return person;
 	}
 	
+	public Person update(Person person) {
+
+		logger.info("Updatin one person");
+		
+		return person;
+	}
+	
+	private Person mockPerson(int i) {
+
+		logger.info("Finding all");
+		Person person = new Person();
+
+		person.setID(counter.incrementAndGet());
+		person.setFirstName("Person name: " + i);
+		person.setLastName("Lasta name: " + 1);
+		person.setAddress("Some address in Brasil: " + i);
+		person.setGender("Male");
+
+		return person;
+
+	}
 }
