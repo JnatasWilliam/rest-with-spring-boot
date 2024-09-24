1678ee84c188bba66c42ac2052f37a4d10c08739
jonatas <jonatas.santos@sigma.com.br>
2024-09-23 23:48:11 -0300

    Removendo a annotation @Repository pois esta deprecated

--- PrimeirosPassos/rest-with-spring-boot/src/main/java/br/com/springjonatas/repositories/PersonRepository.java
+++ PrimeirosPassos/rest-with-spring-boot/src/main/java/br/com/springjonatas/repositories/PersonRepository.java
@@ -1,11 +1,9 @@
 package br.com.springjonatas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.springjonatas.model.entity.Person;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 	
 
