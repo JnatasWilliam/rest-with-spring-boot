615def40ba47577d1b5d457faffc99386dd9cd48
jonatas <jonatas.santos@sigma.com.br>
2024-09-30 23:07:27 -0300

    Adicionando unit teste e mocks do Dozer

--- PrimeirosPassos/rest-with-spring-boot/src/main/java/br/com/springjonatas/mapper/DozerMapper.java
+++ PrimeirosPassos/rest-with-spring-boot/src/main/java/br/com/springjonatas/mapper/DozerMapper.java
@@ -1,5 +1,8 @@
 package br.com.springjonatas.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
 
@@ -9,5 +12,15 @@ public class DozerMapper {
 
 	public static <O, D> D parseObject (O origin, Class<D> destination) {
 		return mapper.map(origin, destination);
+	}
+
+	public static <O, D> List<D> parseListObjects (List<O> origin, Class<D> destination) {
+		List<D> destinationObjcts = new ArrayList<D>();
+		
+		for (O o : origin) {
+			destinationObjcts.add(mapper.map(o, destination));
+		}
+		
+		return destinationObjcts;
 	}
 }
